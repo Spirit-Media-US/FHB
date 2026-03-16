3ba9aba13afe3968876923270aff88e10901f251
Spirit Media Deploy <kevin@spiritmediapublishing.com>
2026-03-16 23:02:36 +0000

    audit: verify all .astro files use dynamic siteSettings, confirm no hardcoded content, validate schema structure

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,6 +1,9 @@
 import scripture from './scripture'
 import projectUpdate from './projectUpdate'
 import partner from './partner'
+import partnerTier from './partnerTier'
+import page from './page'
+import scriptureComparison from './scriptureComparison'
 import siteSettings from './siteSettings'
 
-export const schemaTypes = [scripture, projectUpdate, partner, siteSettings]
+export const schemaTypes = [scripture, projectUpdate, partner, partnerTier, page, scriptureComparison, siteSettings]
